d71bf3623b015c9638581b6afe7caf68fcc03c58
alperyazir <alperyazir66@gmail.com>
2017-09-12 00:43:29 +0300

    -Satıştaki menüler eklendi

--- app/src/main/java/com/example/ayzr/nepismis_v01/OrdersOnSale.java
+++ app/src/main/java/com/example/ayzr/nepismis_v01/OrdersOnSale.java
@@ -1,10 +1,43 @@
 package com.example.ayzr.nepismis_v01;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class OrdersOnSale extends AppCompatActivity {
+    private ProgressDialog progress;
+    private CookActivity.struct_menu menu;
+    private int count_menu_on_sale;
+    private int count_remain_menu;
+    double rating_no;
+
+    TextView meal_1;
+    TextView meal_2;
+    TextView meal_3;
+    ImageView image;
+    RatingBar rating;
+    TextView menu_sale;
+    TextView remain_menu;
+    Button remove_order;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +46,163 @@ public class OrdersOnSale extends AppCompatActivity {
 
         setTitle(R.string.orders_on_sale);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        meal_1 = (TextView) findViewById(R.id.text_on_sale_menu_1);
+        meal_2 = (TextView) findViewById(R.id.text_on_sale_menu_2);
+        meal_3 = (TextView) findViewById(R.id.text_on_sale_menu_3);
+        image = (ImageView) findViewById(R.id.image_on_sale);
+        rating = (RatingBar) findViewById(R.id.rating_on_sale);
+        menu_sale = (TextView) findViewById(R.id.text_count_menu_on_sale);
+        remain_menu = (TextView) findViewById(R.id.text_count_remain_menu_on_sale);
+        remove_order = (Button) findViewById(R.id.button_on_sale_remove);
+
+        parser();
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(getApplicationContext());
+        builder1.setTitle("Onay");
+        builder1.setMessage("Emin miyiz?");
+
+        builder1.setPositiveButton(
+                "Evet",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        remove_order_ready();
+                        dialog.cancel();
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "Hayır",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        final AlertDialog alert11 = builder1.create();
+
+
+        remove_order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // alert11.show();
+                remove_order_ready();
+            }
+        });
     }
+
+    public void parser() {
+        progress = new ProgressDialog(this);
+        progress.setMessage("Checking menus...");
+        progress.setIndeterminate(true);
+        progress.show();
+
+
+        HashMap<String, String> params_db;
+
+        AccountDatabase accountDatabase = new AccountDatabase(getApplicationContext());
+        params_db = accountDatabase.kullaniciDetay();
+
+
+        HttpCall httpCall = new HttpCall();
+        httpCall.setMethodtype(HttpCall.GET);
+        httpCall.setUrl("http://nepismis.afakan.net/android/satisMenu");
+        HashMap<String, String> params = new HashMap<>();
+        params.put("user_id", params_db.get("tarih"));
+        httpCall.setParams(params);
+
+        new HttpRequest() {
+            @Override
+            public List<CookActivity.struct_menu> onResponse(String response) {
+                super.onResponse(response);
+                try {
+                    progress.dismiss();
+
+                    JSONObject initial = new JSONObject(response);
+                    count_menu_on_sale = initial.getInt("ilk_porsiyon");
+                    count_remain_menu = initial.getInt("porsiyon");
+                    rating_no = initial.getDouble("puan");
+                    menu = new CookActivity.struct_menu();
+
+                    JSONArray array_menu = initial.getJSONArray("menusatis");
+                    for (int i = 0; i < array_menu.length(); i++) {
+                        JSONObject ogun_object = array_menu.getJSONObject(i);
+                        menu.meal.add(i, ogun_object.getString("yemek_adi"));
+                        if (i == 1) {
+                            menu.order_picture_id = ogun_object.getInt("yemek_id");
+                        }
+                    }
+                    update_menus();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute(httpCall);
+
+    }
+
+    public void update_menus() {
+
+        meal_1.setText(menu.meal.get(0));
+        meal_2.setText(menu.meal.get(1));
+        meal_3.setText(menu.meal.get(2));
+
+        menu_sale.setText("Satışa Sunulan :" + count_menu_on_sale + " Adet");
+        remain_menu.setText("Kalan : " + count_remain_menu + " Adet");
+
+        rating.setRating((float) rating_no);
+
+        String url = "http://nepismis.afakan.net/images/yemek/" + menu.order_picture_id + ".jpg";
+
+        Picasso.with(getApplicationContext()).load(url).fit().centerCrop()
+                .placeholder(R.mipmap.ic_launcher_ne_pismis)
+                .error(R.mipmap.ic_launcher_ne_pismis)
+                .into(image);
+
+    }
+
+    public void remove_order_ready() {
+        progress = new ProgressDialog(this);
+        progress.setMessage("Kaldırılıyor...");
+        progress.setIndeterminate(true);
+        progress.show();
+
+
+        HashMap<String, String> params_db;
+
+        AccountDatabase accountDatabase = new AccountDatabase(getApplicationContext());
+        params_db = accountDatabase.kullaniciDetay();
+
+
+        HttpCall httpCall = new HttpCall();
+        httpCall.setMethodtype(HttpCall.GET);
+        httpCall.setUrl("http://nepismis.afakan.net/android/satisMenuSil");
+        HashMap<String, String> params = new HashMap<>();
+        params.put("user_id", params_db.get("tarih"));
+        httpCall.setParams(params);
+
+        new HttpRequest() {
+            @Override
+            public List<CookActivity.struct_menu> onResponse(String response) {
+                super.onResponse(response);
+                try {
+                    progress.dismiss();
+
+                    JSONObject initial = new JSONObject(response);
+                    int saved = initial.getInt("save");
+                    if (saved == 1) {
+                        Toast.makeText(getApplicationContext(), "Kaldırıldı :)", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Hata! Kaldıralamadı  :(", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute(httpCall);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         onBackPressed();
--- app/src/main/java/com/example/ayzr/nepismis_v01/RequestService.java
+++ app/src/main/java/com/example/ayzr/nepismis_v01/RequestService.java
@@ -1,15 +1,26 @@
 package com.example.ayzr.nepismis_v01;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class RequestService extends AppCompatActivity {
 
     EditText editText_service_request;
+    private Button button_send_request;
+    private ProgressDialog progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +29,62 @@ public class RequestService extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         editText_service_request = (EditText) findViewById(R.id.editText_request_service);
+        button_send_request = (Button) findViewById(R.id.button_send_request);
 
 
+        button_send_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send_service_request();
+            }
+        });
     }
+
+    public void send_service_request(){
+        progress = new ProgressDialog(this);
+        progress.setMessage("Servis isteniyor ...");
+        progress.setIndeterminate(true);
+        progress.show();
+
+        HashMap<String, String> params_db;
+        AccountDatabase accountDatabase = new AccountDatabase(getApplicationContext());
+        params_db = accountDatabase.kullaniciDetay();
+
+        HttpCall httpCall2 = new HttpCall();
+        httpCall2.setMethodtype(HttpCall.GET);
+        httpCall2.setUrl("http://nepismis.afakan.net/android/servisIste");
+        HashMap<String,String> params = new HashMap<>();
+        params.put("user_id",params_db.get("tarih"));
+        params.put("servis", editText_service_request.getText().toString());
+        httpCall2.setParams(params);
+        new HttpRequest(){
+            @Override
+            public List<CookActivity.struct_menu> onResponse(String response) {
+                super.onResponse(response);
+                try {
+                    progress.dismiss();
+                    JSONObject initial = new JSONObject(response);
+                    int saved = initial.getInt("save");
+                    if (saved == 1) {
+                        Toast.makeText(getApplicationContext(),  editText_service_request.getText().toString() +" Adet Servis İstendi :)", Toast.LENGTH_SHORT).show();
+                        editText_service_request.setText("");
+
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Servis istenirken hata oluştu :(", Toast.LENGTH_SHORT).show();
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+
+                    Toast.makeText(getApplicationContext(),"Başarısız servis isteme!",Toast.LENGTH_SHORT).show();
+
+                }
+                return null;
+            }
+
+        }.execute(httpCall2);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         onBackPressed();
